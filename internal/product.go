722ff6e91774d8b39683635312bb80d708116584
Karim <k.mohamed@hqolsa.com>
2024-03-20 00:37:57 +0200

    c

--- internal/product.go
+++ internal/product.go
@@ -1,13 +1,8 @@
 package internal
 
 type Product struct {
-	Id                 int64
-	Title              string
-	Description        string
-	Price              int64
-	DiscountPercentage float64
-	Rating             float64
-	Stock              int64
-	Brand              string
-	Category           string
+	Id          int64
+	Title       string
+	Description string
+	Price       int64
 }
